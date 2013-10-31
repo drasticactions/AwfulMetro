149d604d50c3032f50e76f8b841a17d3e7641df4
drasticactions <innerlogic4321@gmail.com>
2013-10-31 13:21:47 -0400

    Remove commeted/useless code

--- BackgroundStatus/BackgroundNotifyStatus.cs
+++ BackgroundStatus/BackgroundNotifyStatus.cs
@@ -1,31 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.ApplicationModel.Background;
-using Windows.Data.Xml.Dom;
-using Windows.Security.Credentials;
-using Windows.UI.Notifications;
-using BusinessObjects;
-using BusinessObjects.Entity;
+﻿using BusinessObjects.Entity;
 using BusinessObjects.Manager;
 using BusinessObjects.Tools;
-using Windows.Storage;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
 
 namespace BackgroundStatus
 {
     public sealed class BackgroundNotifyStatus : IBackgroundTask
-    {
-
-        BackgroundTaskDeferral _deferral = null;
-        IBackgroundTaskInstance _taskInstance = null;
-        volatile bool _cancelled = false;
-        StreamContent _currentStream;
-
-       
+    {  
         List<long> ThreadIds = new List<long>();
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
@@ -52,25 +37,6 @@ namespace BackgroundStatus
             }
 
             this.CreateBookmarkLiveTiles(ForumThreads);
-            //deferral.Complete();
-        }
-
-        private void OnCompleted(IBackgroundTaskRegistration task, BackgroundTaskCompletedEventArgs args)
-        {
-            _cancelled = true;
-            if (_currentStream != null)
-            {
-                //_currentStream.s;
-            }
-        }
-
-        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
-        {
-            _cancelled = true;
-            if (_currentStream != null)
-            {
-                //_currentStream.s;
-            }
         }
 
         private void CreateBookmarkLiveTiles(List<ForumThreadEntity> forumThreads)
