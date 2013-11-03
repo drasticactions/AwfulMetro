040dc5874359728bafaddc0bb34fcd1d74a9e886
Daniel Mann <dbmann@gmail.com>
2013-11-03 15:43:32 -0500

    Adding new files that were missed
    VS2013's git support doesn't track new files automatically, awesome!

---
+++ AwfulMetro.Tests.Unit/Helpers/TestHelpers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace AwfulMetro.Tests.Unit.Helpers
+{
+    public static class TestHelpers
+    {
+        // There's no ExpectedExceptionAttribute for Windows Store apps! Why must Microsoft make my life so hard?!
+        public static void AssertThrowsExpectedException<T>(this Action a) where T : Exception
+        {
+            try
+            {
+                a();
+            }
+            catch (T)
+            {
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown");
+        }
+
+        public static async Task AssertThrowsExpectedException<TException>(this Func<Task> a) where TException : Exception
+        {
+            try
+            {
+                await a();
+            }
+            catch (TException)
+            {
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown");
+        }
+    }
+}
