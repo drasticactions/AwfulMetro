5aa78b6e952e8f592f32504f081f8f4514a58dcd
drasticactions <innerlogic4321@gmail.com>
2014-01-07 21:16:10 -0500

    Add Reply count color converter
    The replies since last opened count on the thread list page should now
    be gray if zero. The color should probably be changed to something
    better though, but the logic is there.

---
+++ AwfulMetro/Tools/RepliesSinceLastOpenedColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace AwfulMetro.Tools
+{
+    public class RepliesSinceLastOpenedColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) return new SolidColorBrush(Colors.Black);
+            var count = (int) value;
+            return count > 0 ? new SolidColorBrush(Colors.Black) : new SolidColorBrush(Colors.LightGray);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
