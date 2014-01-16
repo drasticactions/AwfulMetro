810be82a889b5ff5490614752bc0a303ede1feb8
drasticactions <innerlogic4321@gmail.com>
2014-01-16 11:03:34 -0500

    Fix Unit Tests

--- AwfulMetro.Tests.Unit/Mocks/FakeWebManager.cs
+++ AwfulMetro.Tests.Unit/Mocks/FakeWebManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using AwfulMetro.Core.Manager;
 
@@ -7,6 +8,8 @@ namespace AwfulMetro.Tests.Unit.Mocks
     public class FakeWebManager : IWebManager
     {
         public CookieContainer CookiesToReturn { get; set; }
+
+        public HttpResponseMessage PostResponseMessage { get; set; }
         public WebManager.Result ResultToReturn { get; set; }
         public bool IsNetworkAvailable { get; set; }
         public Task<WebManager.Result> DownloadHtml(string uri)
@@ -17,6 +20,11 @@ namespace AwfulMetro.Tests.Unit.Mocks
         public Task<CookieContainer> PostData(string uri, string data)
         {
             return Task.FromResult(this.CookiesToReturn);
+        }
+
+        public Task<HttpResponseMessage> PostFormData(string uri, MultipartFormDataContent form)
+        {
+            return Task.FromResult(this.PostResponseMessage);
         }
 
         public FakeWebManager()
